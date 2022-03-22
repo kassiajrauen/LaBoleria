15bc65476b58a3549746c762bd7abc1746f3c404
KassiaJRauen <kassia.grj@gmail.com>
2022-03-22 20:38:23 -0300

    add feat/clients/:id/orders

--- src/controllers/clientsController.js
+++ src/controllers/clientsController.js
@@ -22,9 +22,47 @@ export async function postClients(req, res) {
             `,
       [name, address, phone]
     );
-    res.sendStatus(201);
+    return res.sendStatus(201);
   } catch (error) {
     console.log(error);
-    res.sendStatus(500);
+    return res.sendStatus(500);
+  }
+}
+
+export async function getOrdersByClientId(req, res) {
+  const { id } = req.params;
+
+  try {
+    const orders = await connection.query(
+      `
+      SELECT 
+      orders.id AS "orderId",
+      orders.quantity,
+      orders."createdAt",
+      orders."totalPrice",
+      cakes.name AS "cakeName"
+      FROM clients
+      JOIN orders ON clients.id=orders."clientId"
+      JOIN cakes ON orders."cakeId"=cakes.id
+      WHERE clients.id=$1
+    `,
+      [id]
+    );
+
+    const ordersByClientId = orders.rows.map((client) => ({
+      orderId: client.orderId,
+      quantity: client.quantity,
+      createdAt: client.createdAt,
+      totalPrice: client.totalPrice,
+      cakeName: client.cakeName,
+    }));
+
+    if (ordersByClientId.length === 0) {
+      return res.sendStatus(404);
+    } else {
+      res.status(200).send(ordersByClientId);
+    }
+  } catch (error) {
+    return res.sendStatus(500);
   }
 }
--- src/routes/clientsRouter.js
+++ src/routes/clientsRouter.js
@@ -1,7 +1,11 @@
 import { Router } from "express";
-import { postClients } from "../controllers/clientsController.js";
+import {
+  postClients,
+  getOrdersByClientId,
+} from "../controllers/clientsController.js";
 
 const clientsRouter = Router();
 clientsRouter.post("/clients", postClients);
+clientsRouter.get("/clients/:id/orders", getOrdersByClientId);
 
 export default clientsRouter;
